a8d2fca3469d3391ba50f9b8024e7430f670484d
Jose <josecarvajalhilario@gmail.com>
2023-10-31 14:02:39 +0100

    Create openStatement method in database service

--- service-database/src/test/java/io/jeasyarch/api/PostgresqlTest.java
+++ service-database/src/test/java/io/jeasyarch/api/PostgresqlTest.java
@@ -1,6 +1,24 @@
 package io.jeasyarch.api;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
 public class PostgresqlTest extends BaseTest {
     @PostgresqlContainer
     static final DatabaseService database = new DatabaseService();
+
+    @Test
+    public void shouldBeAbleToRunQueries() {
+        database.openStatement(statement -> {
+            try {
+                assertTrue(statement.execute("select 1"));
+            } catch (SQLException e) {
+                fail("Query failed with:" + e.getMessage());
+            }
+        });
+    }
 }
